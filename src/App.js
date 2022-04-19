6007b510ea1306ecabe413001908175a045f7612
lim-yubin <yubineric@gmail.com>
2022-04-19 20:00:42 +0900

    Toggle/Tab

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
+import Tab from './components/Tab/Tab';
+import Toggle from './components/Toggle/Toggle';
+import Slider from './components/Slider/Slider'
+import Input from './components/Input/Input';
+import Dropdown from './components/Dropdown/Dropdown';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="component-container">
+
+      <div id="toggle-component">
+        <div className='title'>Toggle</div>
+        <div><Toggle /></div>
+
+      </div>
+
+      <div id="tab-component">
+        <div className='title'>Tab</div>
+        <div> <Tab /></div>
+      </div>
+
+      <div id="slider-component">
+        <div className='title'>Slider</div>
+        <div><Slider /></div>
+      </div>
+
+      <div id="input-component">
+        <div className='title'>Input</div>
+        <div> <Input /></div>
+      </div>
+
+      <div id="dropdown-component">
+        <div className='title'>Dropdown</div>
+        <div><Dropdown /></div>
+      </div>
+
+
+
+
     </div>
+
   );
 }
 
---
+++ src/components/Toggle/Toggle.js
@@ -0,0 +1,33 @@
+import { useState } from "react"
+import "./Toggle.css"
+
+
+
+export default function Toggle() {
+    const [toggleState, setToggleState] = useState(false)
+
+    const onChange = () => {
+        toggleState ? setToggleState(false) : setToggleState(true)
+    }
+
+
+
+    return (
+        <>
+            <div className="toggle-wrap">
+                <input type="checkbox" id="toggle-input" name="toggle-input" onChange={onChange}></input>
+                <label className="toggle-label" htmlFor="toggle-input">
+                    <div className="toggle-state">
+                        <div className={toggleState ? "basic-false" : "basic-true"}>
+                            기본
+                        </div>
+                        <div className={toggleState ? "detail-true" : "detail-false"}>
+                            상세
+                        </div>
+                    </div>
+                </label>
+
+            </div>
+        </>
+    )
+}
